188f0e7b04d2ecfb1cebe7e2fec035076ee7f7ed
dkedinger <daniel@meshbr.com>
2021-07-11 23:32:36 -0500

    Add files via upload

---
+++ iframe.embed.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    if (!window.document.location.hash) {
+        console.log("reloading");
+        window.location = window.location + "#reloaded";
+        window.location.reload(true);
+    }
+
+    $("iframe").outerWidth($(window).innerWidth());
+    $("iframe").outerHeight($(window).innerHeight());
+});
